aca55b576bd8153939d3b9ce6230b6624850a755
sylvie98 <sylviemasengesho5@gmail.com>
2024-03-14 12:02:55 +0200

    Age Calculator

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const birth=document.getElementById("birthdate");
 function calculateAge() {
     var birthdate = new Date(document.getElementById("birthdate").value);
     var today = new Date();
     var age = today.getFullYear() - birthdate.getFullYear();
+    let date=birth.value
+    if(date ===""){
+      document.getElementById("age").innerHTML= "Your Can Enter Your BirthDay Date";
+    }
+    else{
+      document.getElementById("age").innerHTML= "Your age is " + age + " years old.";
+    }
+   
+   
+   
+      
+     
+      
     
-    document.getElementById("age").innerHTML= "Your age is " + age + " years old.";
   }
